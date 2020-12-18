8b3675b42953eae2bf577521ddb61dcf58da8496
igalv <vilenskyigal@gmail.com>
2020-12-18 09:14:51 +0100

    LiveCodeJs

---
+++ index.js
@@ -0,0 +1,34 @@
+let message;
+message = "Vilensky";
+console.log(message);
+console.log(typeof message);
+console.log(message[2]);
+console.log(message[6]);
+let name;
+name = "Igal";
+let age;
+age = 35;
+let year;
+year = 1985;
+console.log(name + " " + age +  year);
+let isMarried;
+isMarried = "true";
+console.log(isMarried);
+let programmingLanguage;
+programmingLanguage = "JavaScript";
+let isFun;
+isFun = "true";
+console.log(programmingLanguage +  + "isFun" + isFun);
+console.log(programmingLanguage[0,4]);
+console.log(programmingLanguage.length);
+
+let isStudent = true;
+isStudent == true ? console.log("Ticket costs €5,00") : console.log("Ticket costs €12,00");
+
+let speedCheck = 50;
+speedCheck > 50
+  ? console.log("you’re going too fast!’.")
+  : console.log("You’re driving below the speed limit, Oma");
+
+
+
