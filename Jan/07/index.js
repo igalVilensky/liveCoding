3db5f73ef7edd6b7857e651e4b9bdd0ab38ee639
igalv <vilenskyigal@gmail.com>
2021-01-07 14:29:22 +0100

    07.01.21

---
+++ Jan/07/index.js
@@ -0,0 +1,48 @@
+let name = "Igal";
+let age = 35.8;
+let fixedAge = Math.ceil(age);
+let nameAge = name + fixedAge;
+//console.log(Math.ceil(fixedAge));
+console.log(nameAge);
+
+
+
+  
+console.log(Math.floor(Math.random()) * 6 + 1);
+
+/*
+
+  // Math.trunk method rounds up or down towards zero.
+  
+  console.log("negative");
+
+  let num = -3.4;
+  console.log(Math.trunc(num));
+  console.log(Math.ceil(num));
+  console.log(Math.floor(num));
+  console.log(Math.round(num));
+
+  console.log("positive");
+
+  let num2 = 3.4;
+  console.log(Math.trunc(num2));
+  console.log(Math.ceil(num2));
+  console.log(Math.floor(num2));
+  console.log(Math.round(num2));
+
+
+ 
+   /* Math.Truncate, which rounds up or down towards zero.
+
+  Math.Ceiling, which rounds up towards positive infinity.
+
+  Math.Floor, which rounds down towards negative infinity.
+
+  Math.Round, which rounds to the nearest integer or specified number of decimal places. */
+
+
+  
+
+
+
+
