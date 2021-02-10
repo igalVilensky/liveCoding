76acfa07ca8f4cd65cb5e61a83bcb15abd9403c2
igalv <vilenskyigal@gmail.com>
2021-02-10 16:13:40 +0100

    end

--- feb/10/index.js
+++ feb/10/index.js
@@ -96,3 +96,67 @@ const keysValues = (obj) => {
 };
 
 console.log(keysValues(valKeySwitch1(person)));
+
+/* In this challenge, a farmer is asking you to tell him how many legs can be counted among all his animals. The farmer breeds three species:
+
+chickens = 2 legs
+cows = 4 legs
+pigs = 4 legs
+The farmer has counted his animals and he gives you a subtotal for each species. 
+
+You have to implement a function that returns the total number of legs of all the animals.
+
+Examples
+animals(2, 3, 5) ➞ 36
+
+animals(1, 2, 3) ➞ 22
+
+animals(5, 2, 8) ➞ 50
+Notes
+Don't forget to return the result.
+The order of animals passed is animals(chickens, cows, pigs).
+Remember that the farmer wants to know the total number of legs and not the total number of animals. */
+
+const chickens = {
+  name: "chicken",
+  legs: 2,
+};
+
+const cows = {
+  name: "cow",
+  legs: 4,
+};
+
+const pigs = {
+  name: "pig",
+  legs: 4,
+};
+
+const animals = { chickens, cows, pigs };
+
+console.log(animals);
+
+const legsCount = (chick, co, pi) => {
+  return (
+    `${chickens.legs} ` * `${chick}` +
+    `${cows.legs}` * `${co}` +
+    `${pigs.legs}` * `${pi}`
+  );
+};
+
+console.log(legsCount(5, 2, 8));
+
+/* You are counting points for a basketball game, given the amount of 2-pointers scored and 3-pointers scored, find the final points for the team and return that value.
+
+Examples
+points(1, 1) ➞ 5
+
+points(7, 5) ➞ 29
+
+points(38, 8) ➞ 100 */
+
+const points = (two, three) => {
+  return two * 2 + three * 3;
+};
+
+console.log(points(38, 8));
