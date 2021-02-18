df6daecfe4f79d21169e3c3910e75bbb8d5a9d40
igalv <vilenskyigal@gmail.com>
2021-02-18 11:19:01 +0100

    first

---
+++ feb/18/index.js
@@ -0,0 +1,92 @@
+// Create a function that takes an array of non-negative integers and strings and return a new array without the strings.
+
+// Examples
+// filterArray([1, 2, "a", "b"]) ➞ [1, 2]
+
+// filterArray([1, "a", "b", 0, 15]) ➞ [1, 0, 15]
+
+// filterArray([1, 2, "aasf", "1", "123", 123]) ➞ [1, 2, 123]
+// Notes
+// Zero is a non-negative integer.
+// The given array only has integers and strings.
+// Numbers in the array should not repeat.
+// The original order must be maintained.
+/* 
+const filterArray = (arr) => {
+  arr = new Set();
+
+  return arr.size;
+};
+
+console.log(filterArray([1, 2, "aasf", "1", "123", 123, "123"])); */
+
+// Create a function which returns the number of true values there are in an array.
+
+// Examples
+// countTrue([true, false, false, true, false]) ➞ 2
+
+// countTrue([false, false, false, false]) ➞ 0
+
+// countTrue([]) ➞ 0
+// Notes
+// Return 0 if given an empty array.
+// All array items are of the type bool (true or false).
+
+const countTrue = (arr) => {
+  let result;
+
+  result = arr.reduce((acc, cur) => {
+    return acc + (cur === true);
+  }, 0);
+
+  return result;
+};
+
+console.log(countTrue([true, false, false, true, false]));
+console.log(countTrue([false, false, false, false, false]));
+console.log(countTrue([]));
+// reduce
+
+let arr2 = ["hi", "hello", "what"];
+
+let test = arr2.reduce((acc, cur) => {
+  return acc + (" " + cur + " ");
+});
+
+console.log(test);
+
+// In a board game, a piece may advance 1-6 tiles forward depending on the number rolled on a six-sided dice.
+
+// If you advance your piece onto the same tile as another player's piece, both of you earn a bonus.
+
+// Given you and your friend's tile number, create a function that returns if it's possible to earn a bonus when you roll the dice.
+
+// Examples
+// possibleBonus(3, 7) ➞ true
+
+// possibleBonus(1, 9) ➞ false
+
+// possibleBonus(5, 3) ➞ false
+// Notes
+// You cannot move backward (which is why example #3 doesn't work).
+// If you are already on the same tile, return false, as you would be advancing away.
+// Expect only positive integer inputs.
+
+const possibleBonus = (x, y) => {
+  let result;
+  let diceMax = 6;
+  if (x > y || x === y) {
+    result = false;
+  } else if (x + diceMax > y) {
+    result = true;
+  } else {
+    result = false;
+  }
+
+  return result;
+};
+
+console.log(possibleBonus(3, 7));
+console.log(possibleBonus(7, 7));
+console.log(possibleBonus(1, 9));
+console.log(possibleBonus(5, 3));
